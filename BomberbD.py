df4c2cc31bb023b54454c13d5ea64ca7536ddf75
Shithil55 <94660715+Shithil55@users.noreply.github.com>
2023-03-16 13:13:49 +0600

    BomberbD.py

--- BomberbD.py
+++ BomberbD.py
@@ -139,9 +139,5 @@ for i in range (amount):
 	resp = requests.post(url6, headers=headers6, data=data6)	
 	print(str(i+1)+green+'.	➙SMS Sent 😈✅')
 	
-print('					')
-print(cyan+'\t\tThanks For Using BomberbD
-
- Bangladesh By Shithil Ahmed ')
  
  
